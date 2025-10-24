5aa92a777f8f1dea8e3d9ee239134b22f88f4982
vi-pincode123 <rvibin99@gmail.com>
2025-10-24 20:46:46 +0530

    abvd

---
+++ src/sampleproject/student/Student.java
@@ -0,0 +1,11 @@
+package sampleproject.student;
+
+public class Student {
+
+	public static void main(String[] args) {
+	System.out.println("gcccchchcc");
+		// TODO Auto-generated method stub
+
+	}
+
+}
